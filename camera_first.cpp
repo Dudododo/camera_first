eee2d8711ebe9ee38fe106ed1283ae545cbb9c3e
Dudododo <a752601260@gmail.com>
2020-03-06 14:37:41 +0800

    添加项目文件。

---
+++ camera_first.cpp
@@ -0,0 +1,38 @@
+﻿#include <opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	VideoCapture cap;
+
+	cap.open(0);
+
+	if (!cap.isOpened())
+	{
+		std::cout << "wrong:video" << std::endl;
+		return -1;
+	}
+
+	double fps = cap.get(CAP_PROP_FPS);
+	std::cout << "fps" << fps << std::endl;
+	while (1)
+	{
+		cv::Mat frame;
+		bool rSucess = cap.read(frame);
+		if (!rSucess)
+		{
+			std::cout << "wrong:fps" << std::endl;
+			break;
+		}
+		else
+		{
+			cv::imshow("frame", frame);
+		}
+		waitKey(30);
+	}
+	return 0;
+}
+
